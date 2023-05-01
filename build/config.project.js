5b937ae1dcddeae0ff96316b967318f801204c7b
Jimb Esser <wasteland@gmail.com>
2023-05-01 16:43:26 -0700

    Cover images and zip build for Itch.io

--- build/config.project.js
+++ build/config.project.js
@@ -1,3 +1,12 @@
 module.exports = function (config) {
   // nothing; pure defaults
+
+  config.extra_index = [{
+    name: 'itch',
+    defines: {
+      ...config.default_defines,
+      PLATFORM: 'web',
+    },
+    zip: true,
+  }];
 };
